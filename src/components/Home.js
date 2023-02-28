e7ee70f1cf1e901b01fe828224de847b0c1b4abd
saransh-khobragade <saransh98@gmail.com>
2023-02-28 10:27:32 +0530

    connected backed to stg

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,40 @@
-import React from 'react';
-import getConfig from '../config/secrets'
+import React,{ useEffect,useState } from 'react';
+import axios from "axios";
 
 export default function Home() {
 
-  const backendBaseURL = getConfig(process.env.NODE_ENV).backendBaseURL
-  const backendUrl = getConfig(process.env.backendUrl)
+  const [data,setData]=useState([])
+
+  useEffect(()=>{
+    (async()=>{
+      const backendUrl = process.env.backendUrl
+      const response = await axios.get(backendUrl)
+      setData(response.data.movies)
+
+    })()
+  },[])
   
   return (
     <div>
-      Hello React for {backendBaseURL}
-      backend url{backendUrl}
+      Hello React App
+      <table border={1}>
+      <tbody>
+        <tr>
+          <th>id</th>
+          <th>title</th>
+          <th>description</th>
+        </tr>
+        { data?data.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.id}</td>
+              <td>{val.title}</td>
+              <td>{val.description}</td>
+            </tr>
+          )
+        }):null}
+        </tbody>
+      </table>
     </div>
   );
 }
